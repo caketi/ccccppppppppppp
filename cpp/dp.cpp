b9cd89fa9016ec82fe42daf2c07cb7be673b6a17
zhao <zjgzlp@gmail.com>
2022-11-12 20:33:28 +0800

    dp规划

--- cpp/dp.cpp
+++ cpp/dp.cpp
@@ -1,2 +1,39 @@
-#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+
+// acwing 1017.摘花生
+ const int N = 110;
+ int n, m;
+ int w[N][N];
+ int f[N][N];
+// 2      2组数据
+// 2 2    花生苗的行数和列数
+// 1 1     第一个数据从北向南描述每行花生苗的情况，第二个数据从西向东描述该行花生数目
+// 3 4
+// 2 3
+// 2 3 4
+// 1 6 5
+
+// 8
+// 16
+ int main(){
+    int T;
+    scanf("%d", &T);
+    while(T--){
+        scanf("%d%d", &n, &m);
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                scanf("%d", &w[i][j]);
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                f[i][j] = max(f[i-1][j], f[i][j-1])+w[i][j];
+            }
+        }
+        printf("%d\n", f[n][m]);
+    }
+    return 0;
+ }
---
+++ readlist.md
@@ -0,0 +1,6 @@
+https://grantjenks.com/docs/sortedcontainers/pycon-2016-talk.html
+https://grantjenks.com/docs/sortedcontainers/djangocon-2015-lightning-talk.html
+
+
+
+[read-list](http://www.grantjenks.com/docs/sortedcontainers/sf-python-2015-lightning-talk.html)
