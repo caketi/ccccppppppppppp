83cc48825189a3a0953c932325517dac989e16e7
zhao <zjgzlp@gmail.com>
2023-01-13 19:35:36 +0800

    deep javascript

---
+++ javascript/作用域提升.js
@@ -0,0 +1,54 @@
+// function foo(){
+//     var a = b = 100
+//     var a = 100
+//     b  = 100
+// }
+// foo()
+// // console.info(a) // undefined
+// console.info(b) //100 
+
+
+// -============
+// var a = 100
+// function foo(){
+//     console.info(a)
+//     return 
+//     var a = 100
+// }
+// foo() // undefined
+
+
+
+// ------------------
+// function foo(){
+//     console.info(n)
+//     var n = 200
+//     console.info(n)
+// }
+// var n = 100
+// foo() // undefined 200 
+// console.info(n) // 100
+
+// --------------------
+// var n = 100
+// function foo(){
+//     n = 200
+// }
+// foo()
+// console.info(n) // 200
+
+// --------------------
+// var n = 100
+
+// function foo1(){
+//     console.info(n)
+// }
+
+// function foo2(){
+//     var n = 200
+//     console.info(n)
+//     foo1()
+// }
+
+// foo2() // 200 100
+// console.info(n) // 100
